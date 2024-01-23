341327a7c52b29b41c9265cfbf9e0dfa07e6c2ed
yeren66 <794493836@qq.com>
2024-01-23 21:39:38 +0800

    update: add code only & simple context generation by gpt4

--- generate_test_gpt4.py
+++ generate_test_gpt4.py
@@ -10,7 +10,7 @@ from openai import OpenAI
 
 # 设置 API key 和 API base URL
 # api_key = "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"
-api_key = "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"
+api_key = "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"
 # base_url = "https://api.132999.xyz/v1"
 
 client = OpenAI(
@@ -118,40 +118,41 @@ def generate_test(json_data, path, number=10):
         if not os.path.exists(second_path):
             os.makedirs(second_path)
         for j in range(3):
-            third_path = second_path + file_path[j]
-            if not os.path.exists(third_path):
-                os.makedirs(third_path)
-            record = []
-            data = prompts[j]
-            with open(third_path + "result.txt", 'w') as file:
-                file.write("Source code: \n\n")
-                file.write(source_code + "\n\n\n")
-                logging.info("\n-------------source code-------------\n " + source_code + "----------------------------------\n")
-                for k in range(number): 
-                    logging.info("No." + str(k + 1) + " generated result for " + third_path + " -- " + "\n")
-                    if j == 1: # 由于gpt4接口昂贵，先跳过full context的生成
-                        output = "null"
-                    else:
+            if j == 1:
+                third_path = second_path + file_path[j]
+                if not os.path.exists(third_path):
+                    os.makedirs(third_path)
+                record = []
+                data = prompts[j]
+                with open(third_path + "result.txt", 'w') as file:
+                    file.write("Source code: \n\n")
+                    file.write(source_code + "\n\n\n")
+                    logging.info("\n-------------source code-------------\n " + source_code + "----------------------------------\n")
+                    for k in range(number): 
+                        logging.info("No." + str(k + 1) + " generated result for " + third_path + " -- " + "\n")
+                        # if j == 1: # 由于gpt4接口昂贵，先跳过full context的生成
+                        #     output = "null"
+                        # else:
                         output = gpt_interface(data)
-                    file.write("No." + str(k + 1) + " generated result --------------------------\n\n")
-                    file.write(output + "\n\n\n")
-                    file.flush()
-                    record.append(output)
-            with open(third_path + "result.json", 'w') as file:
-                record_data = {
-                    "project_name": json_data[i]['project_name'],
-                    "file_name": json_data[i]['file_name'],
-                    "relative_path": json_data[i]['relative_path'],
-                    "execute_path": json_data[i]['execute_path'],
-                    "package": json_data[i]['package'],
-                    "docstring": json_data[i]['docstring'],
-                    "source_code": json_data[i]['source_code'],
-                    "class_name": json_data[i]['class_name'],
-                    "method_name": json_data[i]['method_name'],
-                    "arguments": json_data[i]['argument_name'],
-                    "generate_test": record
-                }
-                json.dump(record_data, file, indent=4)
+                        file.write("No." + str(k + 1) + " generated result --------------------------\n\n")
+                        file.write(output + "\n\n\n")
+                        file.flush()
+                        record.append(output)
+                with open(third_path + "result.json", 'w') as file:
+                    record_data = {
+                        "project_name": json_data[i]['project_name'],
+                        "file_name": json_data[i]['file_name'],
+                        "relative_path": json_data[i]['relative_path'],
+                        "execute_path": json_data[i]['execute_path'],
+                        "package": json_data[i]['package'],
+                        "docstring": json_data[i]['docstring'],
+                        "source_code": json_data[i]['source_code'],
+                        "class_name": json_data[i]['class_name'],
+                        "method_name": json_data[i]['method_name'],
+                        "arguments": json_data[i]['argument_name'],
+                        "generate_test": record
+                    }
+                    json.dump(record_data, file, indent=4)
 
 
 if __name__ == "__main__":
@@ -170,6 +171,16 @@ if __name__ == "__main__":
 
     # print(gpt_interface("hello world"))
 
-# 这儿记录一下，现在跑的是jfreechart，一共6个method
+# 这部分是跑code only和simple context部分
+# 这儿记录一下，现在跑的是jfreechart，一共6个method，顺利
 # 待会跑Java，里面有35个method，跑完之后再跑其他的
 # Java跑的达到接口限制，有的method没有得出结果，根据log观察是31，33，34这三个method存在部分的timeout
+# 之后跑的是Jeecg Boot项目，基本顺利（存在一个timeout，不影响整体内容）（token花费较大，可能是项目差异）
+# 之后跑了lang 和jctools，一切顺利
+# 跑zxing，一切顺利
+# 跑apollo，一切顺利
+# 跑javacv，一切顺利
+# 跑math，一切顺利
+        
+# 这部分是跑full context部分
+# 先跑lang
